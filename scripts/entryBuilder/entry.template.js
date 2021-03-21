60b8356ddd57c6422f43f1f2f32bd9d479b284ad
Sho Yamane <sns.yamane@gmail.com>
2021-03-22 07:18:35 +0900

    fix: 🐛 entryBuilder

--- scripts/entryBuilder/entry.template.js
+++ scripts/entryBuilder/entry.template.js
@@ -1,6 +1,6 @@
 /*
  * このファイルは自動生成されますので手動では更新しないでください。
+ * $ ts-node ./scripts/entryBuilder
  */
-
 <% _.each(components, (component) => { %>
-export * from "./<%= component.typeName %>/<%= component.dirName %>/<%= component.fileName %>";<% }) %>
+export <%= component.importName %> from "./<%= component.typeName %>/<%= component.dirName %>/<%= component.fileName %>";<% }) %>
---
+++ test.ts
@@ -0,0 +1,1 @@
+import { AvatarProps } from "./src";
