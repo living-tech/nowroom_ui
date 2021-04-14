0b34b5bf9dbde32fc2e87ab514e1ec68f76bb671
Sho Yamane <sns.yamane@gmail.com>
2021-04-14 11:06:49 +0900

    fix: 🐛 IconWithTextVertical

--- lib/components/molecules/IconWithTextVertical/Presenter.d.ts
+++ lib/components/molecules/IconWithTextVertical/Presenter.d.ts
@@ -1,6 +1,8 @@
 import { CSSProperties, VFC } from "react";
 import { Color, IconName } from "../../atoms/Icon/Presenter";
+import { Weight as TextWeight } from "../../atoms/Text/Presenter";
 export declare type Size = "sm" | "md";
+export declare type Weight = TextWeight;
 export declare type Props = {
     className?: string;
     color?: Color;
@@ -8,6 +10,7 @@ export declare type Props = {
     label: string;
     size?: Size;
     style?: CSSProperties;
+    weight?: Weight;
 };
 export declare const Presenter: VFC<Props>;
 //# sourceMappingURL=Presenter.d.ts.map
--- src/components/molecules/IconWithTextVertical/Presenter.tsx
+++ src/components/molecules/IconWithTextVertical/Presenter.tsx
@@ -3,8 +3,11 @@ import { CSSProperties, VFC } from "react";
 import { Icon } from "../../atoms/Icon/Default";
 import { Color, IconName } from "../../atoms/Icon/Presenter";
 import { Text } from "../../atoms/Text/Default";
+import { Weight as TextWeight } from "../../atoms/Text/Presenter";
 
 export type Size = "sm" | "md";
+
+export type Weight = TextWeight;
 
 export type Props = {
   className?: string;
@@ -13,9 +16,10 @@ export type Props = {
   label: string;
   size?: Size;
   style?: CSSProperties;
+  weight?: Weight;
 };
 
-export const Presenter: VFC<Props> = ({ className, color, iconName, label, size = "md", style }) => {
+export const Presenter: VFC<Props> = ({ className, color, iconName, label, size = "md", style, weight = "normal" }) => {
   return (
     <div className={`inline-flex flex-col items-center justify-center ${className}`} style={style}>
       <Icon color={color} name={iconName} size={size === "md" ? "lg" : "md"} />
@@ -24,6 +28,7 @@ export const Presenter: VFC<Props> = ({ className, color, iconName, label, size 
         color={color}
         size={size === "md" ? "xs" : "xxs"}
         style={{ marginTop: size === "md" ? 8 : 6 }}
+        weight={weight}
       >
         {label}
       </Text>
