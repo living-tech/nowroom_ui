505f9c4039fb0102bcee2d12c76b55d29c86f7ff
unknown <getstarted3601@gmail.com>
2021-09-02 18:09:32 +0900

    TextBlack 1ni

--- src/components/atoms/Text/Black.tsx
+++ src/components/atoms/Text/Black.tsx
@@ -3,6 +3,8 @@ import { VFC, memo } from "react";
 import { Presenter, Props } from "./Presenter";
 
 export const TextBlack: VFC<Props> = memo(({ children, color = "black", ...props }) => {    
+  console.log('TextBlack');
+  
   return (
     <Presenter color={color} {...props}>
       {children}
