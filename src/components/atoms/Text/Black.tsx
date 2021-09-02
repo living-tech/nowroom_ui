8c07480f65531928d6ce51299dbceda8c3a0eced
unknown <getstarted3601@gmail.com>
2021-09-02 15:08:56 +0900

    TextBlack に console.log を追加

--- src/components/atoms/Text/Black.tsx
+++ src/components/atoms/Text/Black.tsx
@@ -3,6 +3,8 @@ import { VFC, memo } from "react";
 import { Presenter, Props } from "./Presenter";
 
 export const TextBlack: VFC<Props> = memo(({ children, color = "black", ...props }) => {
+  console.log('TextBlack memo');
+  
   return (
     <Presenter color={color} {...props}>
       {children}
